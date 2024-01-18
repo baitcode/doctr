07df8e7039585bc94bc6fd75626b733b72d540ff
baitcode <batiyiv@gmail.com>
2024-01-18 18:49:46 +0000

    additional debugging info

--- doctr/datasets/utils.py
+++ doctr/datasets/utils.py
@@ -78,7 +78,7 @@ def encode_string(
     try:
         return list(map(vocab.index, input_string))
     except ValueError:
-        raise ValueError("some characters cannot be found in 'vocab'")
+        raise ValueError("some characters cannot be found in 'vocab' for {}".format(input_string))
 
 
 def decode_sequence(
